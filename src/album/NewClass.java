4ce5b35bf2a180496c0ff497fd4f9a4e1d078336
AlefDelgado <alef_delgado@hotmail.com>
2025-11-04 22:14:41 -0600

    ejemplo

---
+++ src/album/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package album;
+
+/**
+ *
+ * @author alefdelgado
+ */
+public class NewClass {
+    
+}
