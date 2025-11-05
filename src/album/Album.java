c1e49c2ae29555fb5efb30311190dbf27cb53190
AlefDelgado <alef_delgado@hotmail.com>
2025-11-04 19:18:54 -0600

    Subiendo proyecto

---
+++ src/album/Album.java
@@ -0,0 +1,12 @@
+package album;
+/*
+ *
+ * @author alefdelgado
+ */
+public class Album {
+
+    public static void main(String[] args) {
+        GUI ventana = new GUI();
+    }
+    
+}
---
+++ src/album/GUI.java
@@ -0,0 +1,21 @@
+package album;
+
+import javax.swing.JFrame;
+
+/*
+ *
+ * @author alefdelgado
+ */
+public class GUI extends JFrame
+{
+
+    public GUI() {
+        setTitle("Album");
+        setSize(800, 500);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
