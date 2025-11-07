f093c802299c524f117b5350c9dd70bd874ca7b1
AlefDelgado <alef_delgado@hotmail.com>
2025-11-07 16:31:30 -0600

    update 1

--- src/album/Album.java
+++ src/album/Album.java
@@ -1,12 +1,21 @@
 package album;
+import java.awt.Color;
+import javax.swing.JFrame;
+
 /*
  *
  * @author alefdelgado
  */
+
 public class Album {
 
     public static void main(String[] args) {
         GUI ventana = new GUI();
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(800,500);
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+                
     }
     
 }
--- src/album/GUI.java
+++ src/album/GUI.java
@@ -1,21 +1,88 @@
 package album;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 /*
  *
  * @author alefdelgado
  */
+
 public class GUI extends JFrame
 {
-
+    private JLabel titulo;
+    private JPanel panel;
+    
+    
     public GUI() {
-        setTitle("Album");
-        setSize(800, 500);
-        setLayout(null);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        super("Log in");
+        
+        
+        
+        Componentes();
+        
+        JMenu menuArchivo = new JMenu("This App");
+        menuArchivo.setMnemonic('A'); 
+        
+        panel.revalidate();
+        panel.repaint();
+        JMenuItem aboutThis = new JMenuItem("About");
+        aboutThis.setMnemonic('c');
+        menuArchivo.add(aboutThis); 
+        aboutThis.addActionListener(
+                new ActionListener()
+        {
+
+            
+            public void actionPerformed(ActionEvent evento) {
+                JOptionPane.showMessageDialog(GUI.this,"                       Album Futbol\n                       UAEMex 2025\nCentro Universitario UAEMex Tianguistenco","Acerca de", JOptionPane.PLAIN_MESSAGE);
+            } 
+        } 
+        ); 
+        
+        JMenuItem elementoSalir = new JMenuItem("Exit");
+        elementoSalir.setMnemonic('S'); 
+        menuArchivo.add(elementoSalir); 
+        elementoSalir.addActionListener(
+                new ActionListener()
+        {
+            
+            
+            public void actionPerformed(ActionEvent evento) {
+                System.exit(0); 
+            } 
+        }
+        );
+        
+        JMenuBar barra = new JMenuBar();
+        setJMenuBar(barra);
+        barra.add(menuArchivo);
+        
+    }
+    
+    public void Componentes() {
+        
+        // Panel principal
+        panel = new JPanel();
+        panel.setBackground(Color.BLACK);
+        add(panel, BorderLayout.CENTER);
+        
+        // Tiulo de la ventana
+        titulo = new JLabel();
+        titulo.setText("Iniciar Sesión");
+        titulo.setFont(new Font("Arial", Font.BOLD, 20));
+        titulo.setForeground(Color.white);
+        panel.add(titulo, BorderLayout.NORTH);
     }
     
 }
